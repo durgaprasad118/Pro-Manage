c9dc9d659939eab8432d5eeb7bca4364e22c9f96
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-29 21:50:14 +0530

    Add : added toast on login/register for the first req info

--- Client/src/pages/Login/Login.jsx
+++ Client/src/pages/Login/Login.jsx
@@ -1,100 +1,105 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import { toast } from "sonner";
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 import {
-  LoginInput,
-  PasswordInput,
-  RegisterButton,
-  Welcome,
-} from "../../components";
-import { useLoginMutation } from "../../store/api/api";
-import { setCredentials } from "../../store/slices/authSlice";
-import "./Login.css";
-import { setFlter } from "../../store/slices/filterSlice";
-import { setAnalytics } from "../../store/slices/taskSlice";
+    LoginInput,
+    PasswordInput,
+    RegisterButton,
+    Welcome
+} from '../../components';
+import { useLoginMutation } from '../../store/api/api';
+import { setCredentials } from '../../store/slices/authSlice';
+import { setFlter } from '../../store/slices/filterSlice';
+import './Login.css';
 const Login = () => {
-  const [isHovered, setIsHovered] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
 
-  const handleMouseOver = () => {
-    setIsHovered(true);
-  };
+    const handleMouseOver = () => {
+        setIsHovered(true);
+    };
 
-  const handleMouseOut = () => {
-    setIsHovered(false);
-  };
+    const handleMouseOut = () => {
+        setIsHovered(false);
+    };
 
-  const [loginInput, setLoginInput] = useState({
-    email: "",
-    password: "",
-  });
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const [login, { isLoading }] = useLoginMutation();
-  const submitHandler = async () => {
-    try {
-      const response = await login({
-        email: loginInput.email,
-        password: loginInput.password,
-      });
-      if (response.error) {
-        return toast.error(`${response.error.data.message}`);
-      }
-      dispatch(setCredentials(response.data));
-      dispatch(setFlter("week"));
-      toast.success("Logged in successfully");
-      navigate("/dash/dash");
-      window.location.href = "/dash/dash";
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <div className="dash-container">
-      <Welcome />
-      <div className="form-container">
-        <h3>Login</h3>
-        <div className="inputs-login">
-          <LoginInput
-            placeholder="email"
-            name="email"
-            value={loginInput.email}
-            setValue={setLoginInput}
-            type="text"
-            icon="email"
-          />
-          <PasswordInput
-            value={loginInput.password}
-            name="password"
-            setValue={setLoginInput}
-            placeholder="password"
-            type="password"
-            icon="password"
-          />
+    const [loginInput, setLoginInput] = useState({
+        email: '',
+        password: ''
+    });
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [login, { isLoading }] = useLoginMutation();
+    const submitHandler = async () => {
+        try {
+            const response = await login({
+                email: loginInput.email,
+                password: loginInput.password
+            });
+            if (response.error) {
+                return toast.error(`${response.error.data.message}`);
+            }
+            dispatch(setCredentials(response.data));
+            dispatch(setFlter('week'));
+            toast.success('Logged in successfully');
+            navigate('/dash/dash');
+            window.location.href = '/dash/dash';
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        toast.info(
+            "For the first request kindly wait for 2 mins, cz I'm using free plan of render "
+        );
+    }, []);
+    return (
+        <div className="dash-container">
+            <Welcome />
+            <div className="form-container">
+                <h3>Login</h3>
+                <div className="inputs-login">
+                    <LoginInput
+                        placeholder="email"
+                        name="email"
+                        value={loginInput.email}
+                        setValue={setLoginInput}
+                        type="text"
+                        icon="email"
+                    />
+                    <PasswordInput
+                        value={loginInput.password}
+                        name="password"
+                        setValue={setLoginInput}
+                        placeholder="password"
+                        type="password"
+                        icon="password"
+                    />
+                </div>
+                <div className="buttons-login">
+                    <RegisterButton
+                        onclick={submitHandler}
+                        text="login"
+                        color="#ffffff"
+                        border="none"
+                        bg="#17A2B8"
+                        hovercolor="#ffffff"
+                    />
+                    <p className="noLogin-text">Have no account yet?</p>
+                    <Link style={{ textDecoration: 'none' }} to={'/'}>
+                        <RegisterButton
+                            text="register"
+                            color="#17A2B8"
+                            border="1px solid #17A2B8"
+                            bg="#ffffff"
+                            hovercolor="#ffffff"
+                        />
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div className="buttons-login">
-          <RegisterButton
-            onclick={submitHandler}
-            text="login"
-            color="#ffffff"
-            border="none"
-            bg="#17A2B8"
-            hovercolor="#ffffff"
-          />
-          <p className="noLogin-text">Have no account yet?</p>
-          <Link style={{ textDecoration: "none" }} to={"/"}>
-            <RegisterButton
-              text="register"
-              color="#17A2B8"
-              border="1px solid #17A2B8"
-              bg="#ffffff"
-              hovercolor="#ffffff"
-            />
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
--- Client/src/pages/Register/Register.jsx
+++ Client/src/pages/Register/Register.jsx
@@ -1,106 +1,111 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 import {
-  LoginInput,
-  PasswordInput,
-  RegisterButton,
-  Welcome,
-} from "../../components";
-import "./Register.css";
-import { toast } from "sonner";
-import { useRegisterMutation } from "../../store/api/api";
-import { useNavigate, Link } from "react-router-dom";
+    LoginInput,
+    PasswordInput,
+    RegisterButton,
+    Welcome
+} from '../../components';
+import { useRegisterMutation } from '../../store/api/api';
+import './Register.css';
 const Register = () => {
-  const [inputVal, setInputVal] = useState({
-    name: "",
-    email: "",
-    password: "",
-    cnfpwd: "",
-  });
+    const [inputVal, setInputVal] = useState({
+        name: '',
+        email: '',
+        password: '',
+        cnfpwd: ''
+    });
 
-  const navigate = useNavigate();
-  const [register, { isLoading, isError, error }] = useRegisterMutation();
-  const submitHandler = async (e) => {
-    e.preventDefault();
-    try {
-      if (inputVal.password !== inputVal.cnfpwd) {
-        toast.error("Password not matched");
-      } else {
-        const result = await register({
-          name: inputVal.name,
-          email: inputVal.email,
-          password: inputVal.password,
-        });
-        if (result.error) {
-          toast.error(result.error.data.message);
-        } else {
-          navigate("/login");
-          toast.success("Successfully registered, login Now");
+    const navigate = useNavigate();
+    const [register, { isLoading, isError, error }] = useRegisterMutation();
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            if (inputVal.password !== inputVal.cnfpwd) {
+                toast.error('Password not matched');
+            } else {
+                const result = await register({
+                    name: inputVal.name,
+                    email: inputVal.email,
+                    password: inputVal.password
+                });
+                if (result.error) {
+                    toast.error(result.error.data.message);
+                } else {
+                    navigate('/login');
+                    toast.success('Successfully registered, login Now');
+                }
+            }
+        } catch (error) {
+            console.log(error);
         }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <div className="dash-container">
-      <Welcome />
-      <div className="form-container">
-        <h4>Register</h4>
-        <div className="register-login">
-          <LoginInput
-            name="name"
-            value={inputVal.name}
-            setValue={setInputVal}
-            placeholder="person"
-            type="name"
-            icon="name"
-          />
-          <LoginInput
-            name="email"
-            value={inputVal.email}
-            setValue={setInputVal}
-            placeholder="email"
-            type="text"
-            icon="email"
-          />
-          <PasswordInput
-            placeholder="password"
-            type="password"
-            icon="password"
-            name="password"
-            value={inputVal.password}
-            setValue={setInputVal}
-          />
-          <PasswordInput
-            placeholder="confirm password"
-            type="password"
-            icon="password"
-            value={inputVal.cnfpwd}
-            setValue={setInputVal}
-            name="cnfpwd"
-          />
+    };
+    useEffect(() => {
+        toast.info(
+            "For the first request kindly wait for 2 mins, cz I'm using free plan of render "
+        );
+    }, []);
+    return (
+        <div className="dash-container">
+            <Welcome />
+            <div className="form-container">
+                <h4>Register</h4>
+                <div className="register-login">
+                    <LoginInput
+                        name="name"
+                        value={inputVal.name}
+                        setValue={setInputVal}
+                        placeholder="person"
+                        type="name"
+                        icon="name"
+                    />
+                    <LoginInput
+                        name="email"
+                        value={inputVal.email}
+                        setValue={setInputVal}
+                        placeholder="email"
+                        type="text"
+                        icon="email"
+                    />
+                    <PasswordInput
+                        placeholder="password"
+                        type="password"
+                        icon="password"
+                        name="password"
+                        value={inputVal.password}
+                        setValue={setInputVal}
+                    />
+                    <PasswordInput
+                        placeholder="confirm password"
+                        type="password"
+                        icon="password"
+                        value={inputVal.cnfpwd}
+                        setValue={setInputVal}
+                        name="cnfpwd"
+                    />
+                </div>
+                <div className="buttons-login">
+                    <RegisterButton
+                        onclick={submitHandler}
+                        text="register"
+                        color="#ffffff"
+                        border="none"
+                        bg="#17A2B8"
+                    />
+                    <p className="noLogin-text">Have no account yet?</p>
+                    <Link style={{ textDecoration: 'none' }} to={'/login'}>
+                        <RegisterButton
+                            text="login"
+                            color="#17A2B8"
+                            border="1px solid #17A2B8"
+                            bg="#ffffff"
+                        />
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div className="buttons-login">
-          <RegisterButton
-            onclick={submitHandler}
-            text="register"
-            color="#ffffff"
-            border="none"
-            bg="#17A2B8"
-          />
-          <p className="noLogin-text">Have no account yet?</p>
-          <Link style={{ textDecoration: "none" }} to={"/login"}>
-            <RegisterButton
-              text="login"
-              color="#17A2B8"
-              border="1px solid #17A2B8"
-              bg="#ffffff"
-            />
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Register;
